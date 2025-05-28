6f15761da92ddb274cdd1cb0f8a6cf96306734bd
Q <121518656+qcargile@users.noreply.github.com>
2025-05-28 16:49:42 -0600

    Add files via upload

---
+++ en.lua
@@ -0,0 +1,20 @@
+return {
+  menu_title = "Dmg. Scaling",
+
+  damage_mult_desc = 'Incoming damage multiplier at this level',
+  npc_to_npc = 'Apply to damage from other NPCs',
+  player_explosion_self = 'Apply to damage from explosions and self',
+  
+  multiplier = 'Multiplier',
+  damage_to_npc_mult = 'Damage to NPC Multiplier by Level',
+  damage_to_player_mult = 'Damage to Player Multiplier by Level',
+  npc_other_mults = 'Other Damage to NPC Multipliers',
+
+  npc_rarity_mults = 'Damage to NPC Multipliers by Tier',
+  trash = 'Trash',
+  weak = 'Weak',
+  rare = 'Rare',
+  officer = 'Officer',
+  elite = 'Elite',
+  boss = "Boss"
+}
